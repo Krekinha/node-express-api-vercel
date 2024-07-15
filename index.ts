ec6000910b78f81704804002cff4c7dc40cfb0a8
Krekinha <krekmg@gmail.com>
2024-07-15 16:45:36 -0300

    16:45

---
+++ index.ts
@@ -0,0 +1,41 @@
+//const express = require('express')
+import fastify from 'fastify'
+
+
+const app = fastify();
+
+
+app.get("/", () => {
+  
+  return { "mensagem": "DEU BOM!!!" };
+});
+
+app
+  .listen({
+    host: "0.0.0.0",
+    port: process.env.PORT ? Number(process.env.PORT) : 3333,
+  })
+  .then(() => {
+    console.log("HTTP Server Running");
+  });
+
+  module.exports = app
+
+
+// const app = express()
+// const PORT = 4000
+
+// app.listen(PORT, () => {
+//   console.log(`API listening on PORT ${PORT} `)
+// })
+
+// app.get('/', (req, res) => {
+//   res.send('Hey this is my API running 🥳')
+// })
+
+// app.get('/about', (req, res) => {
+//   res.send('This is my about route..... ')
+// })
+
+// // Export the Express API
+// module.exports = app
